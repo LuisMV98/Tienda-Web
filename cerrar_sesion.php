e4f3d85a0b76be174d78589f70decc2a9a63af4d
Luis Mv <luis1071@live.com.mx>
2022-05-13 12:04:26 -0500

    Destrucción de variables de usuario

--- cerrar_sesion.php
+++ cerrar_sesion.php
@@ -1,6 +1,10 @@
 <?php
   session_start();
   unset($_SESSION["correous"]);
+  unset($_SESSION["nomb"]);
+  unset($_SESSION["dire"]);
+  unset($_SESSION["correous"]);
+  unset($_SESSION["carrito"]);
   session_destroy();
   header("Location: index.php");
   exit;
