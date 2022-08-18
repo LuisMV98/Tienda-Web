30ae05001303dc7b178499c61700e8ba309e6a0b
Luis Mv <luis1071@live.com.mx>
2022-08-18 04:55:36 -0500

    mejoras

--- templates/cabecera.php
+++ templates/cabecera.php
@@ -5,7 +5,7 @@
 	<title>Los bebos | tu tienda en linea</title>
 	<meta name="vieport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/EstilosTienda.css?ts=<?=time()?>" rel="stylesheet"
+    <link href="css/EstilosTienda.css?ts=<?=time()?>" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Playfair&#43;Display:700,900&amp;display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
    
